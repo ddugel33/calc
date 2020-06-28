1ee09f00c4696685a14bfeda9fa1507fd4cbf11b
ddugel33 <ddugel3@naver.com>
2020-06-28 20:58:48 +0900

    숫자가없는경우추

--- calc.py
+++ calc.py
@@ -5,9 +5,15 @@ def application(environ, start_response):
     d=parse_qs(environ['QUERY_STRING'])
     f_num = d.get('f_num',[''])[0]
     s_num = d.get('s_num',[''])[0]
+    sum = 0
+    mul = 0
 
-    sum = int(f_num) + int(s_num)
-    mul = int(f_num) * int(s_num)
+    if ''not in [f_num, s_num]:
+       	a = int(f_num)
+ 	b = int(s_num)
+        sum = a + b
+        mul = a * b
+
 
     response_body = html % {'sum':sum, 'mul':mul}
 
